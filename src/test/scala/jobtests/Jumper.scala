ea3419ce748ac8a73f6d8df6a096fb6314b77127
Gergo Torcsvari <torcsvari.gergo@gmail.com>
2019-10-19 08:27:17 +0200

    made it tailrec

--- src/test/scala/jobtests/Jumper.scala
+++ src/test/scala/jobtests/Jumper.scala
@@ -2,6 +2,8 @@ package jobtests
 
 import org.scalatest.{Matchers, WordSpecLike}
 
+import scala.annotation.tailrec
+
 class Jumper extends WordSpecLike with Matchers {
 
   "Jumper" should {
@@ -26,29 +28,40 @@ class Jumper extends WordSpecLike with Matchers {
 
   def frontToBack(input: List[Int]): Int = {
 
-    def rec(actualList: List[Int], actualJumps: Int, leftovers: List[(Int, List[Int])], actualMin: Int): Int = {
-      def deeperOrReturnWith(num: Int) = {
-        leftovers match {
-          case Nil => num
-          case (newJumps, newlist) :: newLeftovers =>
-            rec(newlist, newJumps, newLeftovers, num)
-        }
-      }
-      actualList match {
-        case Nil =>  deeperOrReturnWith(actualMin)
-        case h :: _ if h == 0 => deeperOrReturnWith(actualMin)
-        case h :: tail if h > tail.size && actualMin == 0 => deeperOrReturnWith(actualJumps + 1)
-        case h :: tail if h > tail.size && actualJumps + 1 < actualMin =>  deeperOrReturnWith(actualJumps + 1)
-        case h :: tail if h > tail.size =>  deeperOrReturnWith(actualMin)
-        case h :: _ =>
-          val tmpLeftovers = (1 to h).map(i => (actualJumps + 1) -> actualList.drop(i)).toList ++ leftovers
-          val (newJumps, newlist) = tmpLeftovers.head
-          val newLeftovers = tmpLeftovers.tail
-          println(s"$actualJumps -> $newLeftovers")
-          rec(newlist, newJumps, newLeftovers, actualMin)
+    @tailrec
+    def rec(in: (List[Int], Int, List[(Int, List[Int])], Int)): Int = {
+      in match {
+        case (actualList: List[Int], actualJumps: Int, leftovers: List[(Int, List[Int])], actualMin: Int) =>
+
+          def deeperOrReturnWith(num: Int) = {
+            leftovers match {
+              case Nil => (Nil, 0, Nil, num)
+              case (newJumps, newlist) :: newLeftovers =>
+                (newlist, newJumps, newLeftovers, num)
+            }
+          }
+
+          actualList match {
+            case Nil if leftovers == Nil => actualMin
+            case Nil => rec(deeperOrReturnWith(actualMin))
+            case h :: _ if h == 0 => rec(deeperOrReturnWith(actualMin))
+            case h :: tail if h > tail.size && actualMin == 0 => rec(deeperOrReturnWith(actualJumps + 1))
+            case h :: tail if h > tail.size && actualJumps + 1 < actualMin => rec(deeperOrReturnWith(actualJumps + 1))
+            case h :: tail if h > tail.size => rec(deeperOrReturnWith(actualMin))
+            case h :: _ =>
+              val tmpLeftovers = (1 to h).map(i => (actualJumps + 1) -> actualList.drop(i)).toList ++ leftovers
+              val (newJumps, newlist) = tmpLeftovers.head
+              val newLeftovers = tmpLeftovers.tail
+              println(s"$actualJumps -> $newLeftovers")
+              rec(newlist, newJumps, newLeftovers, actualMin)
+          }
       }
     }
 
     rec(input, 0, List.empty, 0)
+  }
+
+  def backToFront(input: List[Int]): Int = {
+    ???
   }
 }
