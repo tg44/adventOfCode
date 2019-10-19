6607d244dd7858c47dbe1b8cc417c531cfcbc49c
Gergo Torcsvari <torcsvari.gergo@gmail.com>
2019-10-19 08:18:01 +0200

    first solution

--- src/test/scala/jobtests/Jumper.scala
+++ src/test/scala/jobtests/Jumper.scala
@@ -22,6 +22,33 @@ class Jumper extends WordSpecLike with Matchers {
 
   }
 
-  def solve(input: Seq[Int]): Int = ???
+  def solve(input: Seq[Int]): Int = frontToBack(input.toList)
 
+  def frontToBack(input: List[Int]): Int = {
+
+    def rec(actualList: List[Int], actualJumps: Int, leftovers: List[(Int, List[Int])], actualMin: Int): Int = {
+      def deeperOrReturnWith(num: Int) = {
+        leftovers match {
+          case Nil => num
+          case (newJumps, newlist) :: newLeftovers =>
+            rec(newlist, newJumps, newLeftovers, num)
+        }
+      }
+      actualList match {
+        case Nil =>  deeperOrReturnWith(actualMin)
+        case h :: _ if h == 0 => deeperOrReturnWith(actualMin)
+        case h :: tail if h > tail.size && actualMin == 0 => deeperOrReturnWith(actualJumps + 1)
+        case h :: tail if h > tail.size && actualJumps + 1 < actualMin =>  deeperOrReturnWith(actualJumps + 1)
+        case h :: tail if h > tail.size =>  deeperOrReturnWith(actualMin)
+        case h :: _ =>
+          val tmpLeftovers = (1 to h).map(i => (actualJumps + 1) -> actualList.drop(i)).toList ++ leftovers
+          val (newJumps, newlist) = tmpLeftovers.head
+          val newLeftovers = tmpLeftovers.tail
+          println(s"$actualJumps -> $newLeftovers")
+          rec(newlist, newJumps, newLeftovers, actualMin)
+      }
+    }
+
+    rec(input, 0, List.empty, 0)
+  }
 }
