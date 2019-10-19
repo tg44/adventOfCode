afae92d42f437dc6fa66fd5df36ed9a1cba744a5
Gergo Torcsvari <torcsvari.gergo@gmail.com>
2019-10-19 07:48:38 +0200

    first testSet

--- src/test/scala/jobtests/Jumper.scala
+++ src/test/scala/jobtests/Jumper.scala
@@ -1,5 +1,27 @@
 package jobtests
 
-class Jumper {
+import org.scalatest.{Matchers, WordSpecLike}
+
+class Jumper extends WordSpecLike with Matchers {
+
+  "Jumper" should {
+
+    val cases = Seq(
+      Seq(1) -> 1,
+      Seq(0) -> 0,
+      Seq(1, 1) -> 2,
+      Seq(2, 0) -> 1,
+      Seq(2, 0, 3, 4, 0, 0, 0) -> 3
+    )
+
+    cases.foreach{ case (in, out) =>
+      s"$in should return $out" in {
+        solve(in) shouldBe out
+      }
+    }
+
+  }
+
+  def solve(input: Seq[Int]): Int = ???
 
 }
